7d5167b248f7ebd5133067de70ea7238193dfdcf
shiva shankar <sivas1475@gmail.com>
2022-11-15 14:12:53 +0530

    roboshop

--- components/catalogue.sh
+++ components/catalogue.sh
@@ -20,3 +20,11 @@ ECHO "Download Application Content"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>${LOG_FILE}
 statusCheck $?
 
+ECHO "Extract Application Archive"
+cd /home/roboshop && rm -rf catlogue &>>${LOG_FILE} && unzip /tmp/catalogue.zip &>>${LOG_FILE} && mv catalogue-main catalogue
+statusCheck $?
+
+ECHO "Install NodeJS Modules"
+cd /home/roboshop/catalogue && npm install &>>${LOG_FILE} && chown roboshop:roboshop /home/roboshop/catalogue -R
+statusCheck $?
+
