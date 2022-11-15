14d90c2f25beb2e7390e23f422207d526692ac39
shiva shankar <sivas1475@gmail.com>
2022-11-15 13:55:19 +0530

    roboshop

--- components/mongodb.sh
+++ components/mongodb.sh
@@ -3,3 +3,10 @@
 source components/common.sh
 checkRootUser
 
+ECHO "Setup MongoDB Yum Repo"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>${LOG_FILE}
+statusCheck $?
+
+ECHO "Installing MongoDB"
+yum install -y mongodb-org &>>${LOG_FILE}
+statusCheck $?
