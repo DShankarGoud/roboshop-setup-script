81c016f3cfc42d9220ecf75df24f006b51b1ba19
shiva shankar <sivas1475@gmail.com>
2022-11-15 14:35:28 +0530

    roboshop

--- components/catalogue.sh
+++ components/catalogue.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+source components/common.sh
+checkRootUser
+
 ECHO "Configure NodeJS Yum Repos"
 curl -sL https://rpm.nodesource.com/setup_1ts.x | bash &>>${LOG_FILE}
 statusCheck $?
