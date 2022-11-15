d2fc071d713f03d06ef08e2d0bcb2782a7dcd1be
shiva shankar <sivas1475@gmail.com>
2022-11-15 14:00:42 +0530

    roboshop

--- components/mongodb.sh
+++ components/mongodb.sh
@@ -14,3 +14,21 @@ statusCheck $?
 ECHO "Configure Listen Address in MongoDB Configuration"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 statusCheck $?
+
+ECHO "Start Mongodb Service"
+systemctl restart mongod &>>${LOG_FILE} && systemctl enable mongod &>>{LOG_FILE}
+statusCheck $?
+
+ECHO "Download Schema"
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>${LOG_FILE}
+statusCheck $?
+
+ECHO "Extract Schema Zip"
+cd /tmp && unzip mongodb.zip &>>${LOG_FILE}
+statusCheck $?
+
+cd mongodb-main
+ECHO "Load Schema"
+mongo < catalogue.js &>>${LOG_FILE} && mongo < users.js &>>${LOG_FILE}
+statusCheck $?
+
