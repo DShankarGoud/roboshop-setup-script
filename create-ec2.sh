6b6f989c89968a1046a83eac1e3b58bc645f91bd
shiva shankar <sivas1475@gmail.com>
2022-11-15 13:21:35 +0530

    roboshop

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+  echo "instance name as argument is needed"
+  exit 1
+fi
+
+if [ "$1" == "list" ]; then
+  aws ec2 describe-instances  --query "Reservations[*].Instances[*].{PrivateIP:PrivateIpAddress,PublicIP:PublicIpAddress,Name:Tags[?Key=='Name']|[0].Value,Status:State.Name}"  --output table
+  exit 0
+fi
+
+NAME=$1
+
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" --output table  | grep ImageId | awk '{print $4}')
 
 aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${NAME}}]" "ResourceType=instance,Tags=[{Key=Name,Value=${NAME}}]" --security-group-ids sg-06b080e7d4a4104a6 &>/dev/null
