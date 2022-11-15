d05069f24a2bd405ca12bb2268f3c9898cdc9a7f
shiva shankar <sivas1475@gmail.com>
2022-11-15 13:58:46 +0530

    roboshop

--- components/mongodb.sh
+++ components/mongodb.sh
@@ -10,3 +10,7 @@ statusCheck $?
 ECHO "Installing MongoDB"
 yum install -y mongodb-org &>>${LOG_FILE}
 statusCheck $?
+
+ECHO "Configure Listen Address in MongoDB Configuration"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+statusCheck $?
