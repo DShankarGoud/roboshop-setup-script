10f8a3d3c5b4219883817e60766ec9263dd485eb
shiva shankar <sivas1475@gmail.com>
2022-11-15 12:48:24 +0530

    roboshop

--- components/common.sh
+++ components/common.sh
@@ -3,6 +3,25 @@ checkRootUser() {
 
   if [ "$USER_ID" -ne "0" ]; then
     echo -e "\e[31myou are suppose to be running this script as sudo or root user\e[0m"
-    exit
+    exit 1
   fi
 }
+
+statusCheck() {
+  if [ $1 -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILURE\e[0m"
+    echo "check the error log in ${LOG_FILE}"
+    exit 1
+  fi
+}
+
+LOG_FILE=/tmp/roboshop.log
+rm -f $LOG_FILE
+
+ECHO() {
+  echo -e "========================== $1 ==========================\n" >>${LOG_FILE}
+  echo "$1"
+}
+
--- components/frontend.sh
+++ components/frontend.sh
@@ -1,15 +1,39 @@
 #!/usr/bin/env bash
 
 
-yum install nginx -y
-systemctl enable nginx
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+source components/common.sh
+checkRootUser
+
+
+ECHO "Installing Nginx"
+yum install nginx -y &>>${LOG_FILE}
+statusCheck $?
+
+ECHO "Downloading Frontend Code"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>${LOG_FILE}
+statusCheck $?
+
 cd /usr/share/nginx/html
-rm -rf *
-unzip /tmp/frontend.zip
-mv frontend-main/* .
-mv static/* .
-rm -rf frontend-main README.md
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
-systemctl restart nginx
+
+ECHO "Removing Old Files"
+rm -rf * &>>${LOG_FILE}
+statusCheck $?
+
+ECHO "Extracting Zip Content"
+unzip /tmp/frontend.zip &>>${LOG_FILE}
+statusCheck $?
+
+ECHO "Copying extracted Content"
+mv frontend-main/* . &>>${LOG_FILE} && mv static/* . &>>${LOG_FILE} && rm -rf frontend-main README.md &>>${LOG_FILE}
+statusCheck $?
+
+ECHO "Copy Roboshop Nginx Config"
+mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>${LOG_FILE}
+statusCheck $?
+
+ECHO "Start Nginx Service"
+systemctl enable nginx &>>${LOG_FILE}
+systemctl restart nginx &>>${LOG_FILE}
+statusCheck $?
+
 
